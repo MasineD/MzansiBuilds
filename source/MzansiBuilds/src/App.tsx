80a8f5305628c244c48e638ee4c1c972237c9fa3
MasineD <masinedonald@gmail.com>
2026-04-12 12:05:28 +0200

    development(auth pages):This adds the signup and signin pages with styling

--- source/MzansiBuilds/src/App.tsx
+++ source/MzansiBuilds/src/App.tsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home'
+import Signin from './pages/signin'
+import Signup from './pages/signup'
 
 export default function App() {
 
@@ -8,6 +10,8 @@ export default function App() {
       <div className="">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
     </div>
--- source/MzansiBuilds/src/pages/home.tsx
+++ source/MzansiBuilds/src/pages/home.tsx
@@ -4,8 +4,11 @@ import About from '../components/about';
 import Services from '../components/services';
 import ContactUs from '../components/contactUs';
 import Footer from '../components/footer';
+import { useNavigate } from 'react-router';
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
       <Navbar />
@@ -15,7 +18,7 @@ const Home = () => {
                 <h2 className="text-[70px] text-center font-heading font-bold mb-4">Build in Public,</h2>
                 <p className="mb-8 max-w-2xl mx-auto text-center text-[40px]">Collaborate with Others</p>
             </div>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
+            <button onClick={() => navigate('/signin')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
               Get Started
             </button>
         </div>
