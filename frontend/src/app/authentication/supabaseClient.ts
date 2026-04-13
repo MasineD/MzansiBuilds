f3577f7ad36edd6367a27a295d09c1c49f477d43
MasineD <masinedonald@gmail.com>
2026-04-13 17:52:43 +0200

    recreation(recreating project):This recreates the project from scratch after

--- frontend/src/app/authentication/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = "https://knauxgxfwcahtvadtuun.supabase.co"
-const supabaseKey = "sb_publishable_LtoH1ysWFsJfkdp-Xs_3WQ_tO21VEdA"
-// const supabaseKey = process.env.SUPABASE_KEY
-export const supabase = createClient(supabaseUrl, supabaseKey)
