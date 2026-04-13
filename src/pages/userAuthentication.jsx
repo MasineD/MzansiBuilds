b2e45c35eac11e68d9b5ea0e5b726061d7ec0784
MasineD <masinedonald@gmail.com>
2026-04-13 20:24:03 +0200

    recreation_dev_feature(auth styling):This styles the authentication pages

--- src/pages/userAuthentication.jsx
+++ src/pages/userAuthentication.jsx
@@ -1,12 +1,19 @@
 import React,{ useState, useEffect } from 'react'
 import supabase from '../client'
 import Dashboard from './dashboard'
+import { Link } from 'react-router-dom'
+import { FaHome } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom'
+import '../index.css'
 
 const UserAuthentication = ({setToken}) => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [username, setUsername] = useState('')
   const [user, setUser] = useState(null)
+
+    // Allowing the user to navigate to the home page when they click on the home button
+    const navigate = useNavigate()
 
   // Checking if the user wants to login or register
   const [isLogin, setIsLogin ] = useState(true);
@@ -78,25 +85,32 @@ const UserAuthentication = ({setToken}) => {
     </div>
   ):(
     <>
-    <div>
+    <div className='flex items-center justify-center py-[22dvh]'>
       {!isLogin && (
-        <div className="signupContainer">
-          <h1>Sign Up</h1>
-          <input onChange={(e) => setUsername(e.target.value)} type="text" placeholder="Username" />
-          <input onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email" />
-          <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
-          <button onClick={handleSignUp}>Sign Up</button>
-          <p>Already have an account? <button onClick={()=> setIsLogin(true)}>Sign In</button></p>
+        <div className="authContainer ">
+            <button className="homeButton flex items-center text-black/50 hover:text-black/90 mr-80 transition-colors hover:cursor-pointer" onClick={()=>navigate('/')}>
+                <FaHome className="mr-2"/> Home
+            </button>
+            <h1 className='authHeader'>Sign Up</h1>
+            <input className='authInput' onChange={(e) => setUsername(e.target.value)} type="text" placeholder="Username" />
+            <input className='authInput' onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email" />
+            <input className='authInput' onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
+            <button className='authButton' onClick={handleSignUp}>Sign Up</button>
+            <p>Already have an account? <button className='authLink' onClick={()=> setIsLogin(true)}>Sign In</button></p>
       </div>
       )}
       
       {isLogin && (
-        <div className="loginContainer">
-          <h1>Login</h1>
-          <input onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email" />
-          <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
-          <button onClick={handleLogin}>Login</button>
-          <p>Don't have an account? <button onClick={()=> setIsLogin(false)}>Sign Up</button></p>
+        <div className="authContainer ">
+            <button className="homeButton flex items-center text-black/50 hover:text-black/90 mr-80 transition-colors hover:cursor-pointer" onClick={()=>navigate('/')}>
+                <FaHome className="mr-2"/> Home
+            </button>
+            <h1 className='authHeader'>Sign In</h1>
+            <input className='authInput' onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email" />
+            <input className='authInput' onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
+            <p className='pr-65 my-4 text-black/50 hover:text-black hover:underline transition-colors'><Link>forgot password?</Link></p>
+            <button className='authButton' onClick={handleLogin}>Login</button>
+            <p>Don't have an account? <button className='authLink' onClick={()=> setIsLogin(false)}>Sign Up</button></p>
       </div>
       )}
     </div>
