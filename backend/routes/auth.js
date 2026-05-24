f3dc33028d2b63580c35117ed515e5560ed265bb
Masine Donald <masinedonald@gmail.com>
2026-05-24 18:10:20 +0200

    Change SameSite cookie attribute to 'none'

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -9,7 +9,7 @@ const router = express.Router();
 const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'none',
     maxAge: 30 * 24 * 60 * 60 * 1000
 };
 
